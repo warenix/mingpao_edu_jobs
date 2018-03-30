1f8b7748b3573696f007680d00a9e8a2e1747433
warenix <warenix@gmail.com>
2018-03-30 19:37:07 +0800

    Update parser and get job ad details

--- crawl.py
+++ crawl.py
@@ -7,16 +7,26 @@ __author__ = 'warenix'
 
 
 keyword_list = [
-        u'\u4e2d'.encode('utf-8', 'ignore'), # chinese
+
+        u'\u5e38\u984d'.encode('utf-8', 'ignore'), # regular
+        #u'\u4e2d'.encode('utf-8', 'ignore'), # chinese
+        u'\u5c0f\u5b78'.encode('utf-8', 'ignore'), # primary school
+        u'\u4e2d\u6587'.encode('utf-8', 'ignore'), # chinese subject
         'chinese'.encode('utf-8', 'ignore'),
         u'\u5404\u79d1'.encode('utf-8', 'ignore'), # every subject
         u'\u6587\u6191'.encode('utf-8', 'ignore'), # certified teacher
-        u'\u5e38\u984d'.encode('utf-8', 'ignore'), # regular
         'primary'.encode('utf-8', 'ignore'),
     ]
 
+company_keyword_list = [
+        u'\u5c0f\u5b78'.encode('utf-8', 'ignore'), # primary school
+        'primary'.encode('utf-8', 'ignore'),
+    ]
+
+
 class Link(object):
-    base_url = 'http://jump.mingpao.com/cfm/'
+    #base_url = 'http://jump.mingpao.com/cfm/'
+    base_url = 'https://jump.mingpao.com/job/detail/Jobs/2/'
     label = None
     url = None
 
@@ -41,10 +51,15 @@ class Link(object):
 class JobAd(object):
     position = None
     company = None
+    date = None
+    jobid = None
+    content = None
 
-    def __init__(self, position, company):
+    def __init__(self, jobid, position, company, date):
+        self.jobid = jobid
         self.position = position
         self.company = company
+        self.date = date
 
     def get_position(self):
         return self.position
@@ -52,7 +67,22 @@ class JobAd(object):
     def get_company(self):
         return self.company
 
+    def get_date(self):
+        return self.date
+
+    def get_url(self):
+        return 'https://jump.mingpao.com/job/detail/Jobs/2/' + self.jobid
+
+    def get_content(self):
+        return self.content
+
 def getKey(me, other):
+    # compare company
+    me_company = me.get_company()
+    other_company= other.get_company()
+    result_company = compare_by_keyword(company_keyword_list, me_company, other_company)
+    if result_company != 0:
+        return result_company
 
     # compare position
     me_position = me.get_position()
@@ -61,12 +91,21 @@ def getKey(me, other):
     result_position = compare_by_keyword(keyword_list, me_position, other_position)
     if result_position != 0:
         return result_position
-    return me_position < other_position
+
+    # compare content
+    me_content = me.get_content()
+    other_content = other.get_content()
+    result_content = compare_by_keyword(keyword_list, me_content, other_content)
+    if result_content != 0:
+        return result_content
+
+    return me_content < other_content
+
 
 
 def compare_by_keyword(keyword_list, me, other):
-    me_label = me.get_label().lower()
-    other_label = other.get_label().lower()
+    me_label = me.lower()
+    other_label = other.lower()
 
     for keyword in keyword_list:
         result = check_contain_keyword(keyword, me_label, other_label)
@@ -84,61 +123,7 @@ def check_contain_keyword(keyword, me_label, other_label):
         return -1
     return 0
 
-class MyHTMLParser(HTMLParser):
-    STATE_START = 0
-    STATE_FIND_POSITION = 1
-    STATE_ROW_FOUND = 2
-    STATE_TABLE_EXIT = 3
-    STATE_FIND_COMPANY = 4
-
-    state = STATE_START
-
-    job_ad_list = None
-    position = None
-    company = None
-
-    def handle_starttag(self, tag, attrs):
-        # print "Encountered a start tag:", tag
-        if self.STATE_START == self.state:
-            if self.checkInTag('tr', 'class', 'odd', tag, attrs) or self.checkInTag('tr', 'class', 'even', tag, attrs):
-                self.state = self.STATE_FIND_POSITION
-
-        elif self.STATE_FIND_POSITION == self.state:
-            # if self.checkInTag('a', 'target', '_top', tag, attrs):
-            if tag == 'a' and self.hasAttr('target', attrs):
-                label = self.getFromAttrs('title', attrs)
-                url = self.getFromAttrs('href', attrs)
-                self.position = Link(label, url)
-
-                self.state = self.STATE_FIND_COMPANY
-                #
-                # link = Link(label, url)
-                # print link.get_label_url()
-
-        elif self.STATE_FIND_COMPANY == self.state:
-            if self.checkInTag('a', 'target', '_top', tag, attrs):
-                label = self.getFromAttrs('title', attrs)
-                url = self.getFromAttrs('href', attrs)
-                self.company = Link(label, url)
-
-                self.job_ad_list.append(JobAd(self.position, self.company))
-                self.state = self.STATE_FIND_POSITION
-
-    def handle_endtag(self, tag):
-        if self.state == self.STATE_FIND_POSITION:
-            if 'div' == tag:
-                self.state = self.STATE_START
-        pass
-
-    def handle_data(self, data):
-        # print "Encountered some data  :", data
-        if self.STATE_ROW_FOUND == self.state:
-            data = data.strip()
-            if not data == '':
-                # print data
-                pass
-            self.state = self.STATE_FIND_POSITION
-        pass
+class BaseHTMLParser(HTMLParser):
 
     def checkInTag(self, target_tag, target_attr_name, target_attr_value, tag, attrs):
         if target_tag == tag:
@@ -159,12 +144,113 @@ class MyHTMLParser(HTMLParser):
                 return attr[1]
         return None
 
+class MyHTMLParser(BaseHTMLParser):
+    STATE_START = 0
+    STATE_FIND_POSITION = 1
+    STATE_ROW_FOUND = 2
+    STATE_TABLE_EXIT = 3
+    STATE_FIND_COMPANY = 4
+    STATE_FIND_NEXT_JOB = 5
+    STATE_FIND_NEXT_JOB_ENTERED = 6
+    STATE_FOUND_POSITION = 7
+    STATE_FIND_COMPANY = 8
+    STATE_FOUND_COMPANY = 9
+    STATE_FIND_DATE = 10
+    STATE_FOUND_DATE = 11
+
+    state = STATE_START
+
+    job_ad_list = None
+    position = None
+    company = None
+    date = None
+
+    def handle_starttag(self, tag, attrs):
+        # print "Encountered a start tag:", tag
+        if self.STATE_START == self.state:
+            if self.checkInTag('ul', 'id', 'jobListing', tag, attrs):
+                self.state = self.STATE_FIND_NEXT_JOB
+
+        elif self.STATE_FIND_NEXT_JOB == self.state:
+            # if self.checkInTag('a', 'target', '_top', tag, attrs):
+            if tag == 'li' and self.hasAttr('adid', attrs):
+                jobid = self.getFromAttrs('adid', attrs)
+                # print jobid
+                self.jobid = jobid
+                self.state = self.STATE_FIND_NEXT_JOB_ENTERED
+
+        elif self.STATE_FIND_NEXT_JOB_ENTERED == self.state:
+            if tag == 'div' and self.hasAttr('class', attrs):
+                self.state = self.STATE_FOUND_POSITION
+
+        elif self.STATE_FIND_COMPANY == self.state:
+            if tag == 'div' and self.hasAttr('class', attrs):
+                self.state = self.STATE_FOUND_COMPANY
+
+        elif self.STATE_FIND_DATE == self.state:
+            if tag == 'div' and self.hasAttr('class', attrs):
+                self.state = self.STATE_FOUND_DATE
+
+
+    def handle_endtag(self, tag):
+        if self.state == self.STATE_FIND_NEXT_JOB_ENTERED:
+            if 'li' == tag:
+                self.state = self.STATE_FIND_NEXT_JOB
+
+            if 'ul' == tag:
+                self.state = self.STATE_START
+        pass
+
+    def handle_data(self, data):
+        # print "Encountered some data  :", data
+        if self.STATE_FOUND_POSITION == self.state:
+            data = data.strip()
+            if not data == '':
+                #print data
+                self.position = data
+                self.state = self.STATE_FIND_COMPANY
+                pass
+
+        elif self.STATE_FOUND_COMPANY == self.state:
+            data = data.strip()
+            if not data == '':
+                #print data
+                self.company = data
+                self.state = self.STATE_FIND_DATE
+                pass
+
+        elif self.STATE_FOUND_DATE == self.state:
+            data = data.strip()
+            if not data == '':
+                #print data
+                self.date = data
+
+                # add a job
+                # print 'add a job ' + self.position
+                self.job_ad_list.append(JobAd(
+                    self.jobid,
+                    self.position,
+                    self.company,
+                    self.date
+                    ))
+                self.state = self.STATE_FIND_NEXT_JOB_ENTERED
+                pass
+
+
+
+        pass
+
     def get_job_ad_list(self):
         return self.job_ad_list
 
 
 def fetch_mingpao(page_no):
-    url = 'http://jump.mingpao.com/cfm/JobSearch2.cfm?JobAreaID=10&DaysBefore=1&Sort=1D&PageID=%d' % page_no
+    #url = 'http://jump.mingpao.com/cfm/JobSearch2.cfm?JobAreaID=10&DaysBefore=1&Sort=1D&PageID=%d' % page_no
+    #url = 'https://jump.mingpao.com/job/search/Jobs?Keyword=%s&IndustryID=10&Page=%d' % (u'\u4e2d'.encode('utf-8', 'ignore'), page_no)
+    # keyword: chinese
+    # url = 'https://jump.mingpao.com/job/search/Jobs?Keyword=%E4%B8%AD%E6%96%87&IndustryID=10&Page=' + str(page_no)
+    # url = 'https://jump.mingpao.com/job/search/Jobs?Keyword=&IndustryID=10&Page=' + str(page_no)
+    url = 'https://jump.mingpao.com/job/search/Jobs/2?Keyword=&JobAreaID%5B%5D=10-2&IndustryID=10&SortBy=&Page=' + str(page_no)
 
     response = urllib2.urlopen(url)
     html = response.read()
@@ -173,13 +259,66 @@ def fetch_mingpao(page_no):
     parser = MyHTMLParser()
     parser.job_ad_list = []
     parser.feed(html
-                .decode("big5", 'ignore')
-                .encode('utf-8', 'ignore'))
+                #.decode("big5", 'ignore')
+                #.encode('utf-8', 'ignore')
+                )
 
     # print html.decode("big5", 'ignore').encode('utf-8', 'ignore')
 
     return parser.get_job_ad_list()
 
+class JobDetailParser(BaseHTMLParser):
+    ignore_line_list = [
+        '',
+        'More jobs from this Company',
+        'print',
+        'save',
+        'share',
+        'Apply Now',
+    ]
+
+    STATE_FIND_JOB_DETAIL = 0
+    STATE_FOUND_JOB_DETAIL = 1
+
+    state = STATE_FIND_JOB_DETAIL
+
+    content_line_list = []
+
+    def handle_starttag(self, tag, attrs):
+        if self.state == self.STATE_FIND_JOB_DETAIL:
+            if tag == 'div' and self.checkInTag('div', 'id', 'job_detail', tag, attrs):
+                self.state = self.STATE_FOUND_JOB_DETAIL
+                # print '------------------'
+
+        elif self.state == self.STATE_FOUND_JOB_DETAIL:
+            if tag == 'script':
+                self.state = self.STATE_FIND_JOB_DETAIL
+        pass
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        if self.state == self.STATE_FOUND_JOB_DETAIL:
+            data = data.strip()
+            if data not in self.ignore_line_list:
+                # print data
+                self.content_line_list.append(data)
+        pass
+
+    def get_content(self):
+        return "<br/>".join(self.content_line_list)
+
+
+def fetch_job_detail(url):
+    response = urllib2.urlopen(url)
+    html = response.read()
+    # print html
+
+    parser = JobDetailParser()
+    parser.content_line_list= []
+    parser.feed(html)
+    return parser.get_content()
 
 if __name__ == '__main__':
     job_ad_list = []
@@ -189,14 +328,20 @@ if __name__ == '__main__':
         #print 'fetching page_no ', page_no
 
         result = fetch_mingpao(page_no)
-        #print len(result)
+        # print len(result)
         if len(result) == 0:
             break
         job_ad_list.extend(result)
         page_no += 1
 
-    job_ad_list.sort(cmp=getKey, reverse=True)
+    # fetch job details
+    for job_ad in job_ad_list:
+        content = fetch_job_detail(job_ad.get_url())
+        job_ad.content = content
+        pass
     # exit()
+
+    job_ad_list.sort(cmp=getKey, reverse=True)
 
     print '''
     <html>
@@ -211,23 +356,27 @@ if __name__ == '__main__':
     <section class="card">
     <h1>{company}</p>
     <h2><a href=\"{position_url}\">{position_label}</a></h2>
+    <div>{content}</div>
     </section>
     '''
     # print gen dat
     now = datetime.datetime.now()
     print format.format(company=u'\u6700\u5f8c\u66f4\u65b0'.encode('utf-8', 'ignore'),
                             position_url='',
-                            position_label=now.strftime("%Y-%m-%d %H:%M").encode('utf-8', 'ignore'))
+                            position_label=now.strftime("%Y-%m-%d %H:%M").encode('utf-8', 'ignore'),
+                            content='')
 
     # print keyword card
     print format.format(company=u'\u95dc\u9375\u5b57\u6b21\u5e8f'.encode('utf-8', 'ignore'),
                             position_url='',
-                            position_label=' > '.join(keyword_list))
+                            position_label=' > '.join(keyword_list),
+                            content='')
 
     for job_ad in job_ad_list:
-        print format.format(company=job_ad.get_company().get_label(),
-                            position_url=job_ad.get_position().get_url(),
-                            position_label=job_ad.get_position().get_label())
+        print format.format(company=job_ad.get_company(),
+                            position_url=job_ad.get_url(),
+                            position_label=job_ad.get_position(),
+                            content=job_ad.get_content())
 
     print '''
     </body>
